5bdb147b41a0978c825e1b0893f77efd26535974
Praveen Origanti <opraveen@gmail.com>
2016-10-17 01:06:32 +0100

    Updated instructions for Ubuntu 16.04.

--- aws.sh
+++ aws.sh
@@ -1,60 +1,60 @@
 ### Steps to install hadoop-pivot tracing on AWS ###
 ## Pre-reqs
-sudo -s
+# sudo -s (TODO)
 
 
 ## X11 Forwarding
-apt-get -y update
-apt-get -y dist-upgrade
-apt-get -y install xfce4
-sed -i  's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
-apt-get -y install firefox gedit
+sudo apt-get -y update
+sudo apt-get -y dist-upgrade
+sudo apt-get -y install xfce4
+sudo sed -i  's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
+sudo apt-get -y install firefox gedit git vim-gnome make gcc g++
 
 ## JAVA
-add-apt-repository ppa:webupd8team/java # press enter
-apt-get -y update
+sudo add-apt-repository ppa:webupd8team/java # press enter
+sudo apt-get -y update
 # Oracle JAVA 8 License installation #
-$ echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-apt-get -y install oracle-java8-installer
-update-java-alternatives -s java-8-oracle
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get -y install oracle-java8-installer
+sudo update-java-alternatives -s java-8-oracle
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 
 ## Maven
 wget http://apache.mirror.anlx.net/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
-wget http://apache.mirror.anlx.net/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 tar -zxf apache-maven-3.3.9-bin.tar.gz
-cp -R apache-maven-3.3.9 /usr/local
-ln -s /usr/local/apache-maven-3.3.9/bin/mvn /usr/bin/mvn
-ln -s /usr/local/apache-maven-3.3.9/bin/mvnDebug /usr/bin/mvnDebug
+sudo cp -R apache-maven-3.3.9 /usr/local
+sudo ln -s /usr/local/apache-maven-3.3.9/bin/mvn /usr/bin/mvn
+sudo ln -s /usr/local/apache-maven-3.3.9/bin/mvnDebug /usr/bin/mvnDebug
 #mvn -version
-apt-get -y install make
-apt-get -y install gcc
-apt-get -y install g++
 
 ## Protobuf
-apt-get -y install protobuf-compiler
-curl -# -O https://protobuf.googlecode.com/files/protobuf-2.5.0.tar.gz
+sudo apt-get -y remove protobuf-compiler
+curl -# -LO https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz
 gunzip protobuf-2.5.0.tar.gz
 tar -xvf protobuf-2.5.0.tar
 cd protobuf-2.5.0
-./configure
-make ;make install
+./configure --prefix=/usr
+make
+sudo make install
 cd ..
 
+# Relogin with -X option
+#exit
+#ssh -X -i ..
+
 ## AspectJ 
-#wget 'ftp://ftp.stu.edu.tw/eclipse/tools/aspectj/aspectj-1.8.9.jar'
-#java -jar aspectj-1.8.9.jar 
+wget 'ftp://ftp.stu.edu.tw/eclipse/tools/aspectj/aspectj-1.8.9.jar'
+java -jar aspectj-1.8.9.jar 
 #how to bypass next->next->finish command
 
 ## ANT install
-apt-get -y install ant
+sudo apt-get -y install ant
 
 ## Environment Variables
 export CLASSPATH=/root/aspectj1.8/lib/aspectjrt.jar 
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/root/aspectj1.8/bin
 
 ## Tracing Framework
-apt-get -y install git
 git clone https://github.com/brownsys/tracing-framework
 cd tracing-framework/
 mvn clean package install -DskipTests
